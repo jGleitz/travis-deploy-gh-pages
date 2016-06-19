0d86c97086fd9a1a2d4d8d06ef371fdce15316e7
Joshua Gleitze <git@joshuagleitze.de>
2016-06-19 12:34:32 +0200

    Create README in branches folders
    The README was always put on the top level, even if it was for a branch.
    This was wrong and is now fixed.

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 ##########################################################
-# gh-pages deployment script for Travis CI               #                         
+# gh-pages deployment script for Travis CI               #
 #                                                        #
 # Deploys everything from the provided path to gh-pages  #
 # To be called from the project’s root on Travis CI      #
@@ -39,6 +39,10 @@ cd $PUBLISH
 
 REPO="https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git"
 
+# Deploy the master branch to the top most folder, but all other branches to subfolders
+[ $TRAVIS_BRANCH == "master" ] && pubdir="." || pubdir="branches/$TRAVIS_BRANCH"
+mkdir -p "$pubdir"
+
 function set_git_user {
 	git config user.name "Travis CI"
 	git config user.email "travis@example.org"
@@ -47,7 +51,7 @@ function set_git_user {
 function create_readme {
 	echo "# Artifacts of $REPO_NAME
 Built artifacts of $REPO_NAME$([ $TRAVIS_BRANCH != "master" ] && echo " (from branch $TRAVIS_BRANCH)"), \
-published on [GitHub pages](https://$REPO_OWNER.github.io/$REPO_NAME$([ $TRAVIS_BRANCH == "master" ] || echo "/branches/$TRAVIS_BRANCH"))." > README.md
+published on [GitHub pages](https://$REPO_OWNER.github.io/$REPO_NAME$([ $TRAVIS_BRANCH == "master" ] || echo "/branches/$TRAVIS_BRANCH"))." > "$pubdir/README.md"
 }
 
 # Check if gh-pages already exists
@@ -79,11 +83,7 @@ do
 	git rev-parse --verify origin/$branch > /dev/null 2>&1 || (echo "Removing obsolete branch folder of $branch"; git rm -rfq branches/$branch)
 done
 
-
-# Deploy the master branch to the top most folder, but all other branches to subfolders
-[ $TRAVIS_BRANCH == "master" ] && pubdir="." || pubdir="branches/$TRAVIS_BRANCH"
-mkdir -p "$pubdir"
-cd "$pubdir"
+cd $pubdir
 
 # Remove all content - except the git files and the branches folder
 find . -maxdepth 1 \! \( -name .git -o -name . -o -name branches -o -name README.md \) -exec rm -rf {} \;
